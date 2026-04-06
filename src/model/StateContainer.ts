238eb29e8bf934519ad67d0a4cd9f0eb7fddfb62
pointlesspun <anotherpointlesspun@gmail.com>
2026-04-06 23:50:04 +0200

    feat: extract StateContainer interface

---
+++ src/model/StateContainer.ts
@@ -0,0 +1,14 @@
+import type { State } from './State';
+import type { Transition } from './Transition';
+import type { StateId, TransitionId } from './types';
+
+export interface StateContainer {
+  readonly stateIds: Set<StateId>;
+  readonly transitionIds: Set<TransitionId>;
+  hasState(id: StateId): boolean;
+  addState(state: State): void;
+  deleteState(id: StateId): void;
+  hasTransition(id: TransitionId): boolean;
+  addTransition(t: Transition): void;
+  deleteTransition(id: TransitionId): void;
+}
